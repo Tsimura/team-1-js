d4c1c8befdfe0974a325ec66ee538e39623e318a
Tsimura <84808655+Tsimura@users.noreply.github.com>
2021-12-02 18:09:31 +0000

    Deploying to gh-pages from @ Tsimura/team-1-js@eb1de7c717afdba15fb7efd5f486010d3d15765e 🚀

--- src.5c82a7f9.js
+++ src.5c82a7f9.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/src.9139aab2.js.map
+//# sourceMappingURL=/team-1-js/src.5c82a7f9.js.map
