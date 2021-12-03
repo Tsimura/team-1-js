892516329f66c648215c06ed7dbe44abe7c05d55
ElenaStepanchuk <podolyak365@gmail.com>
2021-12-03 21:40:22 +0200

    Module window
    верстка модального окна

---
+++ src/js/modal_window__card.js
@@ -0,0 +1,8 @@
+const modalTittle = document.querySelector('modal_window__card');
+const modalButton = document.querySelector('modal-window-title');
+
+
+
+function createModalWindow() {
+    console.log(5)
+}
