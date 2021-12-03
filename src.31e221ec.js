a5a7945b348bf5ca80b547e1c313d314cd181cbe
Korotkova-Iuliia <84954734+Korotkova-Iuliia@users.noreply.github.com>
2021-12-03 13:44:17 +0000

    Deploying to gh-pages from @ Tsimura/team-1-js@b9cd965105744cafd06b19b356a8f632802ee974 🚀

--- src.31e221ec.js
+++ src.31e221ec.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-1-js/src.26dcb0d2.js.map
+//# sourceMappingURL=/team-1-js/src.31e221ec.js.map
