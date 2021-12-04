3d007f367f4016be27c699f7bc8b9fd61c94f991
ElenaStepanchuk <podolyak365@gmail.com>
2021-12-04 19:28:44 +0200

    Open and shut modal window card

--- src/js/modal_window__card.js
+++ src/js/modal_window__card.js
@@ -2,22 +2,30 @@ const modalTittle = document.querySelector('modal_window__card');
 const modalButton = document.querySelector('modal-window-title');
 
 
+(() => {
+  const refs = {
+    openModalCardBtn: document.querySelector('[data-modal-card-open]'),
+    closeModalCardBtn: document.querySelector('[data-modal-card-close]'),
+    modalCard: document.querySelector('[data-modal-card]'),
+  };
 
-function createModalWindow() {
-    console.log(5)
-}
-// Закрытие модалки
-// (() => {
-//     const refs = {
-//       openSliderBtn: document.querySelector('[data-slider-open]'),
-//       closeSliderBtn: document.querySelector('[data-slider-close]'),
-//       slider: document.querySelector('[data-slider]'),
-//     };
-  
-//     refs.openSliderBtn.addEventListener('click', toggleSlider);
-//     refs.closeSliderBtn.addEventListener('click', toggleSlider);
-  
-//     function toggleSlider() {
-//       refs.slider.classList.toggle('is-hidden');
-//     }
-// })();
+  refs.openModalCardBtn.addEventListener('click', toggleModalCard);
+  refs.closeModalCardBtn.addEventListener('click', toggleModalCard);
+  document.addEventListener('keydown', toggleModalCardEsc);
+
+  function toggleModalCard() {
+    if (refs.modalCard.classList.contains('is-hidden')) {
+      console.log('Модалка с карточкой фильма открыта');
+      refs.modalCard.classList.remove('is-hidden');
+    } else {
+      console.log('Модалка с карточкой фильма закрыта');
+      refs.modalCard.classList.add('is-hidden');
+    }
+  }
+  function toggleModalCardEsc(event) {
+    if (event.keyCode === 27) {
+      console.log('Модалка с командой закрыта кнопкой "Esc"');
+      refs.modalCard.classList.toggle('is-hidden');
+    }
+  }
+})();
