aa6e4e257bc8d221f330e6d901ec410643f3a70d
AliVik <alvikul1994@gmail.com>
2021-12-04 23:55:03 +0200

    добавляет функцию смены стиля

--- src/js/theme-switcher.js
+++ src/js/theme-switcher.js
@@ -1,4 +1,14 @@
 const switcher = document.querySelector('.switch');
+const darkTheme = document.querySelector('dark-theme');
+const lightTheme = document.querySelector('light-theme');
 
 switcher.addEventListener('change', onSwitch);
+
+function onSwitch(evt) {
+
+    evt.currentTarget.children[2].classList.toggle('visually-hidden');
+    evt.target.removeAttribute('checked');
+    evt.currentTarget.children[1].classList.toggle('visually-hidden');
+}
+
 
