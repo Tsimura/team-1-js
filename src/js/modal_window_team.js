bd0f40239156f8ecb3745af734c463058fc1f396
Tsimura <chappa2828@gmail.com>
2021-12-05 12:01:07 +0200

    add app
    in progress...

--- src/js/modal_window_team.js
+++ src/js/modal_window_team.js
@@ -1,27 +1,28 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+const refs = {
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+};
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-  document.addEventListener('keydown', toggleModalEsc);
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+// let key = null;
 
-  function toggleModal() {
-    if (refs.modal.classList.contains('is-hidden')) {
-      console.log('Модалка з командою відкрита');
-      refs.modal.classList.remove('is-hidden');
-    } else {
-      console.log('Модалка з командою закрита');
-      refs.modal.classList.add('is-hidden');
-    }
+function toggleModal(e) {
+  if (refs.modal.classList.contains('is-hidden')) {
+    // key = document.addEventListener('keydown', toggleModalEsc);
+    console.log('Модалка з командою відкрита');
+
+    refs.modal.classList.remove('is-hidden');
+  } else {
+    console.log('Модалка з командою закрита');
+    refs.modal.classList.add('is-hidden');
   }
-  function toggleModalEsc(e) {
-    if (e.keyCode === 27) {
-      console.log('Модалка з командою закрита кнопкою "Esc"');
-      refs.modal.classList.toggle('is-hidden');
-    }
-  }
-})();
+}
+// function toggleModalEsc(e) {
+//   if (e.keyCode === 27) {
+//     refs.modal.classList.add('is-hidden');
+//     key.removeEventListener();
+//     console.log('Модалка з командою закрита кнопкою "Esc"');
+//   }
+// }
