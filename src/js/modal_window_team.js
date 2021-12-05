76e6c1a55d45848499091f9ec4d512c8473e87fd
Tsimura <chappa2828@gmail.com>
2021-12-05 20:08:27 +0200

    add closeModalWindowEsc

--- src/js/modal_window_team.js
+++ src/js/modal_window_team.js
@@ -4,25 +4,25 @@ const refs = {
   modal: document.querySelector('[data-modal]'),
 };
 
-refs.openModalBtn.addEventListener('click', toggleModal);
-refs.closeModalBtn.addEventListener('click', toggleModal);
-// let key = null;
+refs.openModalBtn.addEventListener('click', openModalWindow);
+refs.closeModalBtn.addEventListener('click', closeModalWindow);
 
-function toggleModal(e) {
-  if (refs.modal.classList.contains('is-hidden')) {
-    // key = document.addEventListener('keydown', toggleModalEsc);
-    console.log('Модалка з командою відкрита');
+function openModalWindow(e) {
+  window.addEventListener('keydown', closeModalWindowEsc);
+  console.log('Модалка з командою відкрита');
+  refs.modal.classList.remove('is-hidden');
+}
 
-    refs.modal.classList.remove('is-hidden');
-  } else {
-    console.log('Модалка з командою закрита');
+function closeModalWindow() {
+  console.log('Модалка з командою закрита');
+  refs.modal.classList.add('is-hidden');
+  window.removeEventListener('keydown', closeModalWindowEsc);
+}
+
+function closeModalWindowEsc(e) {
+  if (e.keyCode === 27) {
     refs.modal.classList.add('is-hidden');
+    console.log('Модалка з командою закрита кнопкою "Esc"');
+    window.removeEventListener('keydown', closeModalWindowEsc);
   }
 }
-// function toggleModalEsc(e) {
-//   if (e.keyCode === 27) {
-//     refs.modal.classList.add('is-hidden');
-//     key.removeEventListener();
-//     console.log('Модалка з командою закрита кнопкою "Esc"');
-//   }
-// }
