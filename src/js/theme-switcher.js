c7ccf1f433d341a5ba1fe197d8f89f02115f6620
AliVik <alvikul1994@gmail.com>
2021-12-05 17:08:34 +0200

    добавляет в функцию замену цвета шрифта при изменении темы

--- src/js/theme-switcher.js
+++ src/js/theme-switcher.js
@@ -3,6 +3,7 @@ const switcherRefs = {
     darkTheme: document.querySelector('dark-theme'),
     lightTheme: document.querySelector('light-theme'),
     body: document.querySelector('body'),
+    filmHeadings: document.getElementsByClassName('film_title'),
 }
 switcherRefs.switcher.addEventListener('change', onSwitch);
 function onSwitch(evt) {
@@ -13,13 +14,17 @@ function onSwitch(evt) {
  
     if (!switcherRefs.switcher.children[0].checked) {
         switcherRefs.body.style.backgroundImage = ' linear-gradient(0deg, rgb(101, 21, 139) 25%, rgb(17, 17, 26) 73%)';
-        console.log(switcherRefs.switcher.children[0].checked)
-      
+        console.log(switcherRefs.filmHeadings[0].style);
+        for (let i = 0; i <= switcherRefs.filmHeadings.length; i += 1){
+            switcherRefs.filmHeadings[i].style.color = '#fff';
+        }
     
-    } else if (switcherRefs.switcher.children[0].checked) {
+    } else{
         switcherRefs.body.style.backgroundColor = '#fff';
         switcherRefs.body.style.backgroundImage = 'none';
-      
+       for (let i = 0; i <= switcherRefs.filmHeadings.length; i += 1){
+            switcherRefs.filmHeadings[i].style.color = '#000';
+        }
     }
 }
 
