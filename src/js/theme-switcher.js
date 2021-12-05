0e55a7d192e8129be0d04c6b44cd6fc148343363
AliVik <alvikul1994@gmail.com>
2021-12-05 16:23:10 +0200

    добавляет функции свитчера замену цвета фона

--- src/js/theme-switcher.js
+++ src/js/theme-switcher.js
@@ -1,14 +1,25 @@
-const switcher = document.querySelector('.switch');
-const darkTheme = document.querySelector('dark-theme');
-const lightTheme = document.querySelector('light-theme');
-
-switcher.addEventListener('change', onSwitch);
-
+const switcherRefs = {
+    switcher: document.querySelector('.switch'),
+    darkTheme: document.querySelector('dark-theme'),
+    lightTheme: document.querySelector('light-theme'),
+    body: document.querySelector('body'),
+}
+switcherRefs.switcher.addEventListener('change', onSwitch);
 function onSwitch(evt) {
-
+    
     evt.currentTarget.children[2].classList.toggle('is-hidden');
     evt.target.removeAttribute('checked');
     evt.currentTarget.children[1].classList.toggle('is-hidden');
+ 
+    if (!switcherRefs.switcher.children[0].checked) {
+        switcherRefs.body.style.backgroundImage = ' linear-gradient(0deg, rgb(101, 21, 139) 25%, rgb(17, 17, 26) 73%)';
+        console.log(switcherRefs.switcher.children[0].checked)
+      
+    
+    } else if (switcherRefs.switcher.children[0].checked) {
+        switcherRefs.body.style.backgroundColor = '#fff';
+        switcherRefs.body.style.backgroundImage = 'none';
+      
+    }
 }
-
 
