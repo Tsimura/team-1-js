ecbe254075c773a55b5b96d482b108855ab7f540
AliVik <alvikul1994@gmail.com>
2021-12-05 19:56:28 +0200

    Update theme-switcher.js

--- src/js/theme-switcher.js
+++ src/js/theme-switcher.js
@@ -7,7 +7,7 @@ const switcherRefs = {
     header: document.querySelector('.header'),
     
 }
-console.log(switcherRefs.header.style)
+
 switcherRefs.switcher.addEventListener('change', onSwitch);
 function onSwitch(evt) {
     
@@ -17,17 +17,22 @@ function onSwitch(evt) {
  
     if (!switcherRefs.switcher.children[0].checked) {
         switcherRefs.body.style.backgroundImage = ' linear-gradient(0deg, rgb(101, 21, 139) 25%, rgb(17, 17, 26) 73%)';
-        console.log(switcherRefs.filmHeadings[0].style);
-        switcherRefs.header.style.backgroundImage = 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(/descktop-index.c45719dd.jpg)';
+        switcherRefs.header.style.backgroundImage = 'linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3) ), url(/descktop-index.c45719dd.jpg)';
+        console.log(window.screen.width)
+
         for (let i = 0; i <= switcherRefs.filmHeadings.length; i += 1){
-            switcherRefs.filmHeadings[i].style.color = '#fff';
+            
+           
+            switcherRefs.filmHeadings[i].style.color = '#fff'
+            
         }
     
     } else{
-        switcherRefs.body.style.backgroundColor = '#fff';
-        switcherRefs.body.style.backgroundImage = 'none';
+        switcherRefs.body.removeAttribute('style');
+        switcherRefs.header.removeAttribute('style');
+        
        for (let i = 0; i <= switcherRefs.filmHeadings.length; i += 1){
-            switcherRefs.filmHeadings[i].style.color = '#000';
+            switcherRefs.filmHeadings[i].style.color = '#000'
         }
     }
 }
