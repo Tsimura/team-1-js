76577cc39b324512b67b4b028e7b8c4cf7b03934
AliVik <alvikul1994@gmail.com>
2021-12-09 00:46:00 +0200

    оптимизирует работу свитчера

--- src/js/theme-switcher.js
+++ src/js/theme-switcher.js
@@ -1,142 +1,54 @@
 import localStorage from "./local-storage";
-
 const switcherRefs = {
-    switcher: document.querySelector('.switch'),
-    body: document.querySelector('body'),
-    header: document.querySelector('.header'),
-    footer: document.querySelector('footer'),
-    footerP: document.querySelectorAll('footer p'),
-    footerLinks: document.querySelectorAll('footer a'),
-    footerHeadings: document.querySelectorAll('footer h2'),
-    footerSVG: document.querySelectorAll('footer svg'),
-    gallery: document.querySelector('.hp__gallery_wrapper'),
-    modalForm: document.querySelector('.film-card-modal-window'),
-    modalBtn: document.querySelector('.close-modal-window-btn'),
-    modalSvg: document.querySelector('.icon-close-modal'),
-    modalSignature: document.querySelector('.modal-window-signature'),
-    modalCharacteristics: document.querySelectorAll('.modal-window-characteristics-value li'),
-    
+     switcher: document.querySelector('.switch'),
+    body: document.body,
 }
-
 const PAGE_THEME = 'page-theme';
 const savedPageTheme = localStorage.load(PAGE_THEME);
-const lightTheme = {
-    name: 'light-theme',
-    'switcherRefs.switcher.children[0].checked':true,
-}
-const darkTheme = {
-    name: 'dark-theme',
-    'switcherRefs.switcher.children[0].checked':false,
-}
-// switcherRefs.switcher.children[0].setAttribute('checked', 'checked');
-// getThemeFromLocalStorage();
+const darkTheme = 'dark'
+const lightTheme = 'light'
+
+
+getThemeFromLocalStorage();
 
 switcherRefs.switcher.addEventListener('change', onSwitch);
 
 
 
-
-
-  function onSwitch(evt) {
+function onSwitch(evt) {
     
-    evt.currentTarget.children[2].classList.toggle('is-hidden');
-    evt.target.removeAttribute('checked');
     evt.currentTarget.children[1].classList.toggle('is-hidden');
+    evt.currentTarget.children[2].classList.toggle('is-hidden');
+
  
-    if (!switcherRefs.switcher.children[0].checked) {
-        addDarkThemeStyles();
+    if (switcherRefs.switcher.children[0].checked) {
+        localStorage.remowe(PAGE_THEME)
+        localStorage.clear()
+        switcherRefs.body.setAttribute('data-theme', 'dark');
+        localStorage.save(PAGE_THEME, darkTheme)
+      
         
-        // localStorage.save(PAGE_THEME, JSON.stringify(darkTheme));
         
-        // getThemeFromLocalStorage()
-        console.log(localStorage.load(PAGE_THEME))
-    } else{
-        removeDarkThemeStyles();
-        // console.log('checked',switcherRefs.switcher.children[0].checked)
-        // localStorage.save(PAGE_THEME, JSON.stringify(lightTheme));
-       
-        // getThemeFromLocalStorage();
-        console.log(localStorage.load(PAGE_THEME))
+    } else {
+        localStorage.remowe(PAGE_THEME)
+        localStorage.clear()
+        switcherRefs.body.setAttribute('data-theme', 'light');
+        localStorage.save(PAGE_THEME, lightTheme)
+      
     }
-  }
-  
-
-
-function addDarkThemeStyles() {
-       switcherRefs.body.style.backgroundImage = ' linear-gradient(0deg, rgb(101, 21, 139) 25%, rgb(17, 17, 26) 73%)';
-        switcherRefs.footer.style.backgroundColor = '#000';
-        switcherRefs.gallery.classList.add('dark-theme__text-color');
-        switcherRefs.modalForm.style.backgroundImage = 'linear-gradient(0deg, rgb(101, 21, 139) 25%, rgb(17, 17, 26) 73%)';
-        switcherRefs.modalBtn.style.backgroundColor = 'rgb(17, 17, 26)';
-        switcherRefs.modalBtn.style.fill = '#ff6b01';
-        changeTextColorByStyle(switcherRefs.footerLinks)
-        changeTextColorByClass(switcherRefs.footerP);
-        changeTextColorByClass(switcherRefs.footerHeadings);
-        changeSvgFill(switcherRefs.footerSVG);
-        switcherRefs.modalSignature.classList.add('dark-theme__text-color');
-        changeTextColorByClass(switcherRefs.modalCharacteristics);
+      
 }
 
+function getThemeFromLocalStorage() {
+    if (savedPageTheme === darkTheme) {
+        const settings = {
+            bodyStyle: switcherRefs.body.setAttribute('data-theme', 'dark'),
+            darkBg: switcherRefs.switcher.children[1].classList.add('is-hidden'),
+            lightBg: switcherRefs.switcher.children[2].classList.remove('is-hidden'),
+            checkBoxValue: switcherRefs.switcher.children[0].checked=true,
 
-function removeDarkThemeStyles() {
-      switcherRefs.gallery.classList.remove('dark-theme__text-color');
-        switcherRefs.modalSignature.classList.remove('dark-theme__text-color');
-        
-        removeAttributeStyle(switcherRefs.body);
-        removeAttributeStyle(switcherRefs.header);
-        removeAttributeStyle(switcherRefs.footer);
-        removeAttributeStyleFromArray(switcherRefs.footerLinks);
-        removeAttributeStyleFromArray(switcherRefs.footerSVG);
-        removeDarkThemeClass(switcherRefs.footerP);
-        removeDarkThemeClass(switcherRefs.footerHeadings);
-        removeAttributeStyle(switcherRefs.modalForm);
-        removeAttributeStyle(switcherRefs.modalBtn);
-        removeAttributeStyle(switcherRefs.modalSvg);
-        removeDarkThemeClass(switcherRefs.modalCharacteristics);
+        }
+        return settings;
+    }
+   return switcherRefs.body.setAttribute('data-theme', 'light');
 }
-
-// function getThemeFromLocalStorage() {
-//     if (savedPageTheme === darkTheme) {
-//         switcherRefs.switcher.children[0].removeAttribute('checked');
-//     }
-    
-// }
-
-
-function changeTextColorByClass(arr) {
-    
-         return arr.forEach(el => {
-           
-            return el.classList.add('dark-theme__text-color');
-     })
-    
-   
-}
-  function changeTextColorByStyle(elem) {
-    return elem.forEach(el => {
-        return el.style.color = '#fff';
-    })
-}
-  function changeSvgFill(svg) {
-    return svg.forEach(el => {
-        return el.style.fill = '#fff';
-    })
-}
-  function removeAttributeStyle(elem) {
-    return elem.removeAttribute('style');
-        
-}
-  function removeAttributeStyleFromArray(arr) {
-    return arr.forEach(el => {
-        return el.removeAttribute('style');
-    })
-}
-
-function removeDarkThemeClass(elem) {
-    return elem.forEach(el => {
-        return el.classList.remove('dark-theme__text-color');
-
-    })
-}
-
-
