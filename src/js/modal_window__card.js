bd350c2df56ecd8b926129fab89d6fc1ae895ac9
ElenaStepanchuk <podolyak365@gmail.com>
2021-12-09 10:31:07 +0200

    basiclightbox 1

--- src/js/modal_window__card.js
+++ src/js/modal_window__card.js
@@ -6,34 +6,13 @@ import { makeGenres } from "./makeGenres"
 
 import * as basicLightbox from 'basiclightbox'
 
-// const moduleWindow = document.querySelector(`.insert-module-windows`);
+films.onclick = () => {
+	basicLightbox
+		.create(
+			`<div class="module">
+      <section class="modal-window-section">
 
-  const refs = {
-   closeModalCardBtn: document.querySelector('[data-modal-card-close]'),
-    modalCard: document.querySelector('[data-modal-card]'),
-  };
-
-
- //   films.addEventListener('click', handleModalCardOpen);
-// refs.closeModalCardBtn.addEventListener('click', handleModalCardClose);
-  
-
-  
-
-const options = {
-  onShow: () => {
-    window.addEventListener('keydown', handleKeydown);
-        
-  },
-  onClose: () => {
-    window.removeEventListener('keydown', handleKeydown);
-        
-  },
-};
-
-const instance = basicLightbox.create(`<section class="modal-window-section">
-<div class="backdrop" data-modal-card>
-    <div class="film-card-modal-window">
+		<div class="film-card-modal-window">
     <button class="close-modal-window-btn" data-modal-card-close>
 <svg class="icon-close-modal">
     <use class="icon-close-modal-svg" href="./image/modal-window-card/symbol-defs.svg#icon-close-black">
@@ -69,32 +48,99 @@ been double crossed – but by who and how?
 <button class="btn-queue-modal-window">add to queue</button>
 </div>
 </div>
-</div>
+</section>
+    </div>`)
+		.show();
+};
 
-</section>`, options);
+
+
+// const moduleWindow = document.querySelector(`.insert-module-windows`);
+
+  
+
+
+films.addEventListener('click', handleModalCardOpen);
+refs.closeModalCardBtn.addEventListener('click', handleModalCardClose);
+  
+
+  
+
+// const options = {
+//   onShow: () => {
+//     window.addEventListener('keydown', handleKeydown);
+        
+//   },
+//   onClose: () => {
+//     window.removeEventListener('keydown', handleKeydown);
+        
+//   },
+// };
+
+
+
+// const instance = basicLightbox.create(`
+// <div class="modal">
+//     <div class="film-card-modal-window">
+//     <button class="close-modal-window-btn" data-modal-card-close>
+// <svg class="icon-close-modal">
+//     <use class="icon-close-modal-svg" href="./image/modal-window-card/symbol-defs.svg#icon-close-black">
+//     </use>
+// </svg>
+//     </button>
+// <img class="film-exemple-photo" src="./image/modal-window-card/mobil-modal-card-photo.jpg" alt="#">
+// <div class="modal-window-signature">
+// <h2 class="film-title-modal-window">A FISTFUL OF LEAD</h2>
+// <div class="window-characteristics">
+// <ul class="modal-window-characteristics">
+//     <li class="modal-window-vote list">Vote&nbsp;/&nbsp;Votes</li>
+//     <li class="modal-window-popularity list">Popularity</li>
+//     <li class="modal-winwow-original-title list">Original Title</li>
+//     <li class="modal-window-gener list">Genre</li>
+// </ul>
+// <ul class="modal-window-characteristics-value">
+//     <li class="modal-window-vote-value list"><span class="modal-window-vote__span">7.3</span>/&nbsp;&nbsp;1260</li>
+//     <li class="modal-window-popularity-value list">100.2</li>
+//     <li class="modal-winwow-original-title-value list">A FISTFUL OF LEAD</li>
+//     <li class="modal-window-gener-value list">Western</li>
+// </ul>
+// </div>
+// <h3 class="modal-window-about">About</h3>
+// <p class="modal-window-description">Four of the West’s most infamous outlaws assemble to steal a huge stash of gold from the most corrupt settlement of the
+// gold rush towns. But not all goes to plan one is killed and the other three escapes with bags of gold hide out in the
+// abandoned gold mine where they happen across another gang of three – who themselves were planning to hit the very same
+// bank! As tensions rise, things go from bad to worse as they realise the bags of gold are filled with lead... they’ve
+// been double crossed – but by who and how?
+// </p>
+// <div class="btn-menu-module-card">
+// <button class="btn-watched-modal-window">add to Watched</button>
+// <button class="btn-queue-modal-window">add to queue</button>
+// </div>
+// </div>
+// </div>`, options);
 
 // films.insertAdjacentHTML('beforeend', instance);
-films.addEventListener('click', hendleGalleryClick);
+// films.addEventListener('click', hendleGalleryClick);
 
-function hendleGalleryClick(event) {
-  event.preventDefault();
-  console.log(event.target)
-      console.dir(event.target)
-films.insertAdjacentHTML('beforeend', instance);
-  if (event.target.nodeName !== 'IMG') return;
+// function hendleGalleryClick(event) {
+//   event.preventDefault();
+//   console.log(event.target)
+//       console.dir(event.target)
+// films.insertAdjacentHTML('beforeend', instance);
+//   if (event.target.nodeName !== 'IMG') return;
  
   // const getUrlImage = event.target.dataset.source;
   // console.log(getUrlImage);
   // setOriginalUrl(event.target.dataset.source);
   // currentIndex = Number(event.target.dataset.id);
   
-  instance.show();
+//   instance.show();
 
-}
+// }
     
 
-function handleKeydown({ key }) {
-  // switch (key) {
+// function handleKeydown({ key }) {
+//   switch (key) {
   //   case 'ArrowRight':
   //     currentIndex += 1;
     
@@ -114,33 +160,31 @@ function handleKeydown({ key }) {
   //     setOriginalUrl(galleryItems[currentIndex].original);
   //     break;
     
-  //   case 'Escape':
-      // instance.close();
-    //   break;
+//     case 'Escape':
+//       instance.close();
+//       break;
     
-    // default:
-    //   alert('Что-то пошло не так!');
-  // }
-}
+//     default:
+//       alert('Что-то пошло не так!');
+//   }
+// }
 
-function setOriginalUrl(url) {
-  instance.element().querySelector("img").src = url;
-  return;
-  }
-// function handleModalCardOpen(event) {
+// function setOriginalUrl(url) {
+//   instance.element().querySelector("img").src = url;
+//   return;
+//   }
+function handleModalCardOpen(event) {
     // if (refs.modalCard.classList.contains('is-hidden')) {
-    //   document.addEventListener('keydown', handleModalCardEsc);
+      document.addEventListener('keydown', handleModalCardEsc);
     //   refs.modalCard.classList.remove('is-hidden');
-    //   console.log('Модалка с карточкой фильма открыта');
-      // console.log(event.target)
-      // console.dir(event.target)
+      console.log('Модалка с карточкой фильма открыта');
+      console.log(event.target)
+      console.dir(event.target)
       // let clickWindow = event.target;
       // getFilms(clickWindow).then(createMod)
      
-
-      
          
-// }
+}
 
 // getFilms(page)
 //   .then(createFilmoteka)
