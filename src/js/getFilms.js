bea17778b5c6e1bee2e2708ed567e0bd7de154b0
MaksStelmakh <step4ick2017@gmail.com>
2021-12-10 15:45:41 +0200

    Зробив декілька поправок та переписав fetch функцію на axios

--- src/js/getFilms.js
+++ src/js/getFilms.js
@@ -1,10 +1,25 @@
-export function getFilms(page) {
+import axios from "axios";
+
+export async function getFilms(page) {
   const BASE_URL = `https://api.themoviedb.org/3`;
   const API_KEY = `api_key=221ed015def0321f18a85f3fc7b4d6fa`;
-  return fetch(`${BASE_URL}/trending/movie/week?${API_KEY}&page=${page}`).then(response => {
-    if (!response.ok) {
-      throw new Error(`Error fetching data`);
-    }
-    return response.json();
-  });
-}
+  try {
+    const { data } = await axios.get(`${BASE_URL}/trending/movie/week?${API_KEY}&page=${page}`)
+    return data
+  } catch (error) { 
+    error => console.log(error)
+  }     
+  };
+
+
+
+// export async function getFilms(page) {
+//   const BASE_URL = `https://api.themoviedb.org/3`;
+//   const API_KEY = `api_key=221ed015def0321f18a85f3fc7b4d6fa`;
+//   return fetch(`${BASE_URL}/trending/movie/week?${API_KEY}&page=${page}`).then(response => {
+//     if (!response.ok) {
+//       throw new Error(`Error fetching data`);
+//     }
+//     return response.json();
+//   });
+// }
