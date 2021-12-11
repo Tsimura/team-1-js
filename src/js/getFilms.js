5230584893e652fb338490b758042a54222f2939
MaksStelmakh <step4ick2017@gmail.com>
2021-12-11 13:44:06 +0200

    Переписав деякі функції та зробив код чистішим

--- src/js/getFilms.js
+++ src/js/getFilms.js
@@ -1,8 +1,11 @@
 import axios from "axios";
+import Notiflix from 'notiflix';
+import { createFilmoteka } from "./trending_films";
+
+const BASE_URL = `https://api.themoviedb.org/3`;
+const API_KEY = `api_key=221ed015def0321f18a85f3fc7b4d6fa`;
 
 export async function getFilms(page) {
-  const BASE_URL = `https://api.themoviedb.org/3`;
-  const API_KEY = `api_key=221ed015def0321f18a85f3fc7b4d6fa`;
   try {
     const { data } = await axios.get(`${BASE_URL}/trending/movie/week?${API_KEY}&page=${page}`)
     return data
@@ -11,12 +14,42 @@ export async function getFilms(page) {
   }     
   };
 
+export async function searchFilms(searchForm, page) { 
+  try {
+    const { data } = await axios.get(`${BASE_URL}/search/movie?${API_KEY}&language=en-US&query=${searchForm}&page=1&include_adult=false`)
+    console.log(searchForm.length)
+    if (data.results.length === 0) {
+      Notiflix.Notify.failure('Search result not successful. Enter the correct movie name and ');
+      getFilms(page)
+        .then(createFilmoteka)
+        .catch(error => console.log(error));
+    } else { 
+     return data
+    }
+  } catch (error) { 
+    error => console.log(error)
+  }
+}
 
-
-// export async function getFilms(page) {
+// export function getFilms(page) {
 //   const BASE_URL = `https://api.themoviedb.org/3`;
 //   const API_KEY = `api_key=221ed015def0321f18a85f3fc7b4d6fa`;
 //   return fetch(`${BASE_URL}/trending/movie/week?${API_KEY}&page=${page}`).then(response => {
+//     if (!response.ok) {
+//       throw new Error(`Error fetching data`);
+//     }
+//     return response.json();
+//   });
+// }
+
+
+
+//  export function searchFilms(searchForm) {
+//   const BASE_URL = `https://api.themoviedb.org/3`;
+//   const API_KEY = `api_key=221ed015def0321f18a85f3fc7b4d6fa`;
+//   return fetch(
+//     `${BASE_URL}/search/movie?${API_KEY}&language=en-US&query=${searchForm}&page=1&include_adult=false`,
+//   ).then(response => {
 //     if (!response.ok) {
 //       throw new Error(`Error fetching data`);
 //     }
--- src/js/trending_films.js
+++ src/js/trending_films.js
@@ -3,6 +3,7 @@ import { makeGenres } from './makeGenres';
 import Notiflix from 'notiflix';
 import poster from '../image/posters/poster.jpg';
 import { lazyLoad } from './lazyLoad';
+import { searchFilms } from "./getFilms";
 
 export const films = document.querySelector(`#gallery`);
 const input = document.querySelector(`#search-form`);
@@ -17,18 +18,12 @@ export function createData(page) {
     .catch(error => console.log(error));
 }
 createData(page);
+
 export function createFilmoteka(resp) {
   console.log(resp);
-  if (resp.results.length === 0) {
-    Notiflix.Notify.failure('Search result not successful. Enter the correct movie name and ');
-    getFilms(page)
-      .then(createFilmoteka)
-      .catch(error => console.log(error));
-  } else {
     resp.results.map(data => {
-      return films.insertAdjacentHTML(beforeend, articles(data));
+    return films.insertAdjacentHTML(`beforeend`, articles(data));
     });
-  }
   const img = document.querySelectorAll('#gallery img');
   lazyLoad(img);
 }
@@ -40,7 +35,7 @@ export function articles({ poster_path, original_title, release_date, genre_ids,
      ${
        poster_path
          ? `<img class="hp__gallery_img" id="${id}". src="" data-lazy="https://image.tmdb.org/t/p/w500${poster_path}" loading="lazy" alt="${original_title}"`
-         : `<img class="hp__gallery_img" id="${id}". src="${poster}" data-lazy="  alt="Poster is missing"`
+         : `<img class="hp__gallery_img" id="${id}". src="" data-lazy="${poster}" loading="lazy" alt="Poster is missing"`
      }>
 
       <h2 class="film_title">${original_title}</h2>
@@ -55,33 +50,9 @@ export function articles({ poster_path, original_title, release_date, genre_ids,
     </div>`;
 }
 
-// закомітила функцію....................................................
-// function createFilmoteka(resp) {
-//     console.log(resp)
-//     if (resp.results.length === 0) {
-//         Notiflix.Notify.failure("Search result not successful. Enter the correct movie name and ")
-//         getFilms().then(createFilmoteka).catch(error => console.log(error))
-//     } else {
-//        resp.results.map((data) => {
-//        return films.insertAdjacentHTML(`beforeend`, articles(data))
-//     })
-//     }
-// }
-// закомітила функцію....................................................
-// export function articles({ poster_path, original_title, release_date, genre_ids }) {
-//   return `<div id="gallery" class="hp__gallery_el">
-//       ${
-//         poster_path
-//           ? `<img class="hp__gallery_img" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${original_title}"`
-//           : `<img class="hp__gallery_img" src="${poster}" alt="Poster is missing"`
-//       }>
-//       <h2 class="film_title">${original_title}</h2>
-//       <p class="film_genre">${makeGenres(genre_ids)} | <span>${release_date.substr(0, 4)}</span></p>
-//     </div>`;
-// }
-
 function onSearch(evt) {
   searchForm = evt.currentTarget.elements.searchQuery.value;
+  console.log(searchForm)
   evt.preventDefault();
   clearContainer();
   if (searchForm.length === 0) {
@@ -89,9 +60,9 @@ function onSearch(evt) {
     getFilms(page)
       .then(createFilmoteka)
       .catch(error => console.log(error));
-    return;
+      return;
   }
-  searchFilms(searchForm)
+  searchFilms(searchForm, page)
     .then(createFilmoteka)
     .catch(error => console.log(error));
 }
@@ -100,26 +71,5 @@ function clearContainer() {
   return (films.innerHTML = ``);
 }
 
-function searchFilms(searchForm) {
-  const BASE_URL = `https://api.themoviedb.org/3`;
-  const API_KEY = `api_key=221ed015def0321f18a85f3fc7b4d6fa`;
-  return fetch(
-    `${BASE_URL}/search/movie?${API_KEY}&language=en-US&query=${searchForm}&page=1&include_adult=false`,
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(`Error fetching data`);
-    }
-    return response.json();
-  });
-}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// films.addEventListener('click', function (e) {
-//   e.preventDefault()
-//   const info = e.path[1]
-//   const poster = info.childNodes[1].src
-//   const name = info.childNodes[3].textContent
-//   const genre = info.childNodes[5].childNodes[0].textContent.slice(0, -2)
-//   const year = info.childNodes[5].childNodes[1].textContent
-//  });
