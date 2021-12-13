f7a26b8c934bd7210123d03c42327c2db18825aa
ElenaStepanchuk <podolyak365@gmail.com>
2021-12-13 12:45:30 +0200

    закрытие модалки правка 1

--- src/js/modal_window__card.js
+++ src/js/modal_window__card.js
@@ -103,9 +103,9 @@ function getGanres(data) {
 function markUpModal({ poster_path, title, vote_average, vote_count, popularity, original_title, overview, genre, id }) {
   let localWatched = storage.load('watchedArray') || []
   let localQueue = storage.load('queueArray') || []
-    basicLightbox
-      .create(
-        `<div class="modal">
+  const instance = basicLightbox
+    .create(
+      `<div class="modal">
      <div class="film-card-modal-window">
      <button class="close-modal-window-btn" data-modal-card-close>
 <svg class="icon-close-modal">
@@ -143,107 +143,28 @@ function markUpModal({ poster_path, title, vote_average, vote_count, popularity,
        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     </div>
-</div>`).show();
-  }
+</div>`, {
+      onShow: () => {
+        window.addEventListener('keydown', handleKeydown);
+        
+      },
+      onClose: () => {
+        window.removeEventListener('keydown', handleKeydown);
+        console.log('Модалка с карточкой фильма закрыта кликом вне области модалки');
+      },
+    });
 
-  
-// function markUpModal({ poster_path, title, release_date, vote_average, vote_count, popularity, original_title, overview, id, genre_ids }) {
-//   getYouTube(title, release_date).then(modal)
-//   function modal(data) { 
-//     const videoOnYouTube = data.items[0].id.videoId
-//   basicLightbox
-//       .create(
-//         `<div class="modal">
-//      <div class="film-card-modal-window">
-//      <button class="close-modal-window-btn" data-modal-card-close>
-// <svg class="icon-close-modal">
-//     <use class="icon-close-modal-svg" href="./image/modal-window-card/symbol-defs.svg#icon-close-black">
-//     </use>
-// </svg>
-//     </button>
-// <img class="film-exemple-photo" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="Фото фільму">
-//     <div class="modal-window-signature">
-//     <h2 class="film-title-modal-window">${title}</h2>
-//     <div class="window-characteristics">
-//     <ul class="modal-window-characteristics">
-//     <li class="modal-window-vote list">Vote&nbsp;/&nbsp;Votes</li>
-//     <li class="modal-window-popularity list">Popularity</li>
-//     <li class="modal-winwow-original-title list">Original Title</li>
-//     <li class="modal-window-gener list">Genre</li>
-//     </ul>
-//     <ul class="modal-window-characteristics-value">
-//     <li class="modal-window-vote-value list"><span class="modal-window-vote__span">${vote_average}</span>/&nbsp;&nbsp;${vote_count}</li>
-//     <li class="modal-window-popularity-value list">${popularity}</li>
-//     <li class="modal-winwow-original-title-value list">${original_title}</li>
-//     <li class="modal-window-gener-value list">{makeGenres(genre_ids)}</li>
-// </ul>
-// </div>
-// <h3 class="modal-window-about">About</h3>
-// <p class="modal-window-description">${overview}</p>
-// <div class="btn-menu-module-card">
-// <button id="${id}" class="btn-watched-modal-window">${localWatched.includes(id) ? `remove from watched` : `add to watched`}</button>
-// <button id="${id}" class="btn-queue-modal-window">${localQueue.includes(id) ? `remove from queue` : `add to queue`}</button>
-//          </div>
-//         </div>
-//         <div>
-//          <iframe class="youtube" src="https://www.youtube.com/embed/${videoOnYouTube}"
-//         frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-//         </div>
-//     </div>
-// </div>`).show();
-// } 
-// }
-
-
-
-
-// const options = {
-//   onShow: () => {
-//     window.addEventListener('keydown', handleKeydown);
-//     // data.map({ poster_path, title, vote_average, vote_count, popularity, original_title, overview, id })
+  instance.show()
+  function handleKeydown({ key }) {
+    switch (key) {
+        case 'Escape':
+        instance.close();
+        console.log('Модалка с карточкой фильма закрыта кликом по кнопке "ESC"');
+        break;
     
-//   },
-//   onClose: () => {
-//     window.removeEventListener('keydown', handleKeydown);
-//     console.log('Модалка с карточкой фильма закрыта кликом вне области модалки');   
-//   },
-// };
+      default:
+        alert('Что-то пошло не так!');
+    }
 
-
-
-
-// const instance = basicLightbox.create(`
-// <div class="modal">
-//     <div class="film-card-modal-window">
-//         <button class="close-modal-window-btn" data-modal-card-close>
-//             <svg class="icon-close-modal">
-//                 <use class="icon-close-modal-svg" href="./image/modal-window-card/symbol-defs.svg#icon-close-black">
-//                 </use>
-//             </svg>
-//         </button>
-//         <img class="film-exemple-photo" src="https://image.tmdb.org/t/p/w500{data[poster_path]}" alt="Фото фільму">
-//     <div class="modal-window-signature">
-//     <h2 class="film-title-modal-window">{data.title}</h2>
-//     <div class="window-characteristics">
-//     <ul class="modal-window-characteristics">
-//     <li class="modal-window-vote list">Vote&nbsp;/&nbsp;Votes</li>
-//     <li class="modal-window-popularity list">Popularity</li>
-//     <li class="modal-winwow-original-title list">Original Title</li>
-//     <li class="modal-window-gener list">Genre</li>
-//     </ul>
-//     <ul class="modal-window-characteristics-value">
-//     <li class="modal-window-vote-value list"><span class="modal-window-vote__span">{data.vote_average}</span>/&nbsp;&nbsp;${vote_count}</li>
-//     <li class="modal-window-popularity-value list">{data.popularity}</li>
-//     <li class="modal-winwow-original-title-value list">{data.original_title}</li>
-//     <li class="modal-window-gener-value list">{makeGenres(genre_ids)}</li>
-// </ul>
-// </div>
-// <h3 class="modal-window-about">About</h3>
-// <p class="modal-window-description">{data.overview}</p>
-//             <div class="btn-menu-module-card">
-//                 <button id="{data.id}" class="btn-watched-modal-window">add to Watched</button>
-//                 <button id="{data.id}" class="btn-queue-modal-window">add to queue</button>
-//             </div>
-//         </div>
-//     </div>
-//  </div>`, options);
+  }
+}
