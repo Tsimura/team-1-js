07308b66ea248a9c7c72876e88e4fb856ed288f7
Tsimura <chappa2828@gmail.com>
2021-12-15 22:30:24 +0200

    debugged links and photo

--- src/js/modal_window_team.js
+++ src/js/modal_window_team.js
@@ -9,12 +9,10 @@ refs.closeModalBtn.addEventListener('click', closeModalWindow);
 
 function openModalWindow(e) {
   window.addEventListener('keydown', closeModalWindowEsc);
-  console.log('Модалка з командою відкрита');
   refs.modal.classList.remove('is-hidden');
 }
 
 function closeModalWindow() {
-  console.log('Модалка з командою закрита');
   refs.modal.classList.add('is-hidden');
   window.removeEventListener('keydown', closeModalWindowEsc);
 }
@@ -22,7 +20,6 @@ function closeModalWindow() {
 function closeModalWindowEsc(e) {
   if (e.keyCode === 27) {
     refs.modal.classList.add('is-hidden');
-    console.log('Модалка з командою закрита кнопкою "Esc"');
     window.removeEventListener('keydown', closeModalWindowEsc);
   }
 }
