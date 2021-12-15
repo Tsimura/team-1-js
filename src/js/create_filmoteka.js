ebd27f5f8e442a58eccf50f57d03419d238888f4
Korotkova-Iuliia <3589900@gmail.com>
2021-12-15 14:41:27 +0200

    pagination.off

--- src/js/create_filmoteka.js
+++ src/js/create_filmoteka.js
@@ -77,57 +77,68 @@ export function createFilmoteka(data) {
   films.insertAdjacentHTML('beforeend', createFilmoteka);
   const img = document.querySelectorAll('#gallery img');
   lazyLoad(img);
+  // пагинация........ CALL PAGINATION...............................................
+  paginationTrend();
+  // ................................................
 }
+// ......f() PAGINATION..............................
+function paginationTrend(page) {
+  const options = {
+    totalItems: 1000,
+    visiblePages: '',
+    centerAlign: true,
+    template: {
+      page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+      currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+      moveButton:
+        '<a href="#" class="tui-page-btn tui-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        '</a>',
+      disabledMoveButton:
+        '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        '</span>',
+      moreButton:
+        '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+        '<span class="tui-ico-ellip">...</span>' +
+        '</a>',
+    },
+  };
+  mediaPagination();
+  const container = document.getElementById('pagination');
+  // ...INICIAL......................................................
+  const paginationTrend = new Pagination(container, options);
+  // .............................................................
+
+  // ...PAGINATION ON..............................................
+  paginationTrend.on('afterMove', ({ page }) => {
+    withLoader.addLoader();
+    mediaPagination();
+
+    reset();
+    createData(page);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+  // ..............................................................
+  // ............f() for media.................
+  function mediaPagination() {
+    if (window.innerWidth <= 480) {
+      options.visiblePages = 4;
+    } else {
+      options.visiblePages = 7;
+    }
+  }
+  // // ...PAGINATION OFF.....можно использовать эту .......................................
+  // paginationTrend.off(container, ({ page }) => {
+  //   return createData(page);
+  // });
+  // //  ......  или эту. ......................
+  // paginationTrend.off(container, createData(page));
+  // // .............................................................
+}
+// .END PAGINATION..........................................
+
+//.......... RESET........................................................
 function reset() {
   return (films.innerHTML = '');
-}
-
-// пагинация ...............................................
-// paginationTrend();
-// ..........................................
-
-// пагинация ...............................................
-// paginationTrend();
-// ..........................................
-
-const options = {
-  totalItems: 1000,
-  visiblePages: '',
-  centerAlign: true,
-  template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-    moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</a>',
-    disabledMoveButton:
-      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</span>',
-    moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-      '<span class="tui-ico-ellip">...</span>' +
-      '</a>',
-  },
-};
-mediaPagination();
-const container = document.getElementById('pagination');
-const pagination = new Pagination(container, options);
-page = pagination.getCurrentPage();
-// createData(page, totalPages);
-pagination.on('afterMove', ({ page }) => {
-  withLoader.addLoader();
-  mediaPagination();
-
-  reset();
-  createData(page);
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-});
-function mediaPagination() {
-  if (window.innerWidth <= 480) {
-    options.visiblePages = 4;
-  } else {
-    options.visiblePages = 7;
-  }
 }
