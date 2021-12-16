4881fbbbe0087c7c0bc68168d7fa051f632cf733
Korotkova-Iuliia <3589900@gmail.com>
2021-12-16 12:03:53 +0200

    pagination watch

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -3,100 +3,143 @@ import 'tui-pagination/dist/tui-pagination.css';
 import { films } from './library';
 import { reset } from './library';
 
-// // пагинация для Librery .........................
-// export function paginationWatched(localWatched) {
-//   let page;
-//   let visiblePages = 3;
-//   console.log(localWatched);
-//   console.log(localWatched.length);
-//   if (localWatched.length <= 2) {
-//     page = 1;
-//   }
-//   page = Math.floor(localWatched.length / 2);
-//   console.log(page);
-//   console.log(localWatched);
-//   if (visiblePages > page) {
-//     visiblePages = page;
-//   }
-//
-// const options = {
-//     page,
-//     itemsPerPage: 2,
-//     centerAlign: true,
-//     visiblePages,
-//     template: {
-//       page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-//       currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-//       moveButton:
-//         '<a href="#" class="tui-page-btn tui-{{type}}">' +
-//         '<span class="tui-ico-{{type}}">{{type}}</span>' +
-//         '</a>',
-//       disabledMoveButton:
-//         '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-//         '<span class="tui-ico-{{type}}">{{type}}</span>' +
-//         '</span>',
-//       moreButton:
-//         '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-//         '<span class="tui-ico-ellip">...</span>' +
-//         '</a>',
-//     },
-//   };
+// // пагинация для Librery paginationWatched .........................
+// let page;
+// let visiblePages = 7;
+// const itemsPerPage = 2;
 
-//   // const container = document.getElementById('tui-pagination-container');
-//   const container = document.getElementById('pagination');
-//   const pagination = new Pagination(container, options);
-
-//   pagination.getCurrentPage();
-//   pagination.on('afterMove', page => {
-//     reset();
-//     showFilmsWatched(localWatched);
-//     window.scrollTo({ top: 0, behavior: 'smooth' });
-//   });
+// console.log(localWatched);
+// console.log(localWatched.length);
+// if (localWatched.length <= itemsPerPage) {
+//   page = 1;
+//   // paginationWatched.off(container, showFilmsWatched(localWatched));
 // }
+// page = Math.floor(localWatched.length / itemsPerPage);
+// console.log(page);
+// console.log(localWatched);
+// if (visiblePages > page) {
+//   visiblePages = page;
+// }
+// console.log(page);
+// const options = {
+//   page,
+//   itemsPerPage,
+//   centerAlign: true,
+//   visiblePages,
+//   template: {
+//     page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+//     currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+//     moveButton:
+//       '<a href="#" class="tui-page-btn tui-{{type}}">' +
+//       '<span class="tui-ico-{{type}}">{{type}}</span>' +
+//       '</a>',
+//     disabledMoveButton:
+//       '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+//       '<span class="tui-ico-{{type}}">{{type}}</span>' +
+//       '</span>',
+//     moreButton:
+//       '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+//       '<span class="tui-ico-ellip">...</span>' +
+//       '</a>',
+//   },
+// };
 
-// export function paginationTrend() {
-//   let page = 1;
-//   const options = {
-//     totalItems: 1000,
-//     visiblePages: '',
-//     centerAlign: true,
-//     template: {
-//       page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-//       currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-//       moveButton:
-//         '<a href="#" class="tui-page-btn tui-{{type}}">' +
-//         '<span class="tui-ico-{{type}}">{{type}}</span>' +
-//         '</a>',
-//       disabledMoveButton:
-//         '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-//         '<span class="tui-ico-{{type}}">{{type}}</span>' +
-//         '</span>',
-//       moreButton:
-//         '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-//         '<span class="tui-ico-ellip">...</span>' +
-//         '</a>',
-//     },
-//   };
+// const container = document.getElementById('pagination');
+// const paginationWatched = new Pagination(container, options);
 
-//   mediaPagination();
+// paginationWatched.getCurrentPage();
+// paginationWatched.on('afterMove', localWatched => {
+//   reset();
+//   // pagination.classList.add('is-hidden');
+//   localWatched = storage.load('watchedArray');
+//   films.removeAttribute('style');
 
-//   const container = document.getElementById('pagination');
-//   const pagination = new Pagination(container, options);
-//   page = pagination.getCurrentPage();
-//   // createData(page, totalPages);
-//   pagination.on('afterMove', ({ page }) => {
-//     withLoader.addLoader();
-//     mediaPagination();
-
-//     reset();
-//     createData(page);
-//     window.scrollTo({ top: 0, behavior: 'smooth' });
+//   if (!localWatched || (!localWatched[0] && !localWatched[1])) {
+//     films.style.cssText = `grid-template-columns: repeat(1, 1fr); widht: 100%; height: 100%; margin: 0 auto; justify-items: center;`;
+//     films.innerHTML = `<img class="img-for-library" src='${popcornImg}'  loading="lazy">`;
+//     return Notiflix.Notify.info(`Oops, you haven't added anything to the watched ones yet.`, {
+//       position: 'center-top',
+//     });
+//   }
+//   Notiflix.Notify.success(`You have ${localWatched.length} movies on your list of watched`, {
+//     position: 'center-top',
 //   });
-//   function mediaPagination() {
-//     if (window.innerWidth <= 480) {
-//       options.visiblePages = 4;
-//     } else {
-//       options.visiblePages = 7;
-//     }
+
+//   for (let id of localWatched) {
+//     fetchById(id).then(film => {
+//       renderFilms(film);
+//     });
+//   }
+//   window.scrollTo({ top: 0, behavior: 'smooth' });
+// });
+
+// end пагинация для Librery paginationWatched.....................................................
+
+// пагинация paginationSuarch.....................................................
+
+// const options = {
+//   totalItems: 1000,
+//   visiblePages: '',
+//   centerAlign: true,
+//   template: {
+//     page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+//     currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+//     moveButton:
+//       '<a href="#" class="tui-page-btn tui-{{type}}">' +
+//       '<span class="tui-ico-{{type}}">{{type}}</span>' +
+//       '</a>',
+//     disabledMoveButton:
+//       '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+//       '<span class="tui-ico-{{type}}">{{type}}</span>' +
+//       '</span>',
+//     moreButton:
+//       '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+//       '<span class="tui-ico-ellip">...</span>' +
+//       '</a>',
+//   },
+// };
+// mediaPagination();
+// // const paginationTrend = new Pagination(container, options);
+// page = paginationTrend.getCurrentPage();
+
+// paginationTrend.on('afterMove', ({ page, totalPages }) => {
+//   reset();
+//   mediaPagination();
+//   withLoader.addLoader();
+//   paginationBtn.classList.add('visually-hidden');
+//   window.scrollTo({ top: 0, behavior: 'smooth' });
+//   setTimeout(() => {
+//     return getFilms(page, totalPages)
+//       .then(({ data }) => {
+//         createFilmoteka(data.results);
+//       })
+//       .then(withLoader.removeLoader())
+//       .then(paginationBtn.classList.remove('visually-hidden'))
+//       .catch(error => console.log(error));
+//   }, 2000);
+// });
+
+// // paginationTrend.off(container, ({ page, totalPages }) => {
+// //   reset();
+// //   mediaPagination();
+// //   withLoader.addLoader();
+// //   paginationBtn.classList.add('visually-hidden');
+// //   window.scrollTo({ top: 0, behavior: 'smooth' });
+// //   setTimeout(() => {
+// //     return getFilms(page, totalPages)
+// //       .then(({ data }) => {
+// //         createFilmoteka(data.results);
+// //       })
+// //       .then(withLoader.removeLoader())
+// //       .then(paginationBtn.classList.remove('visually-hidden'))
+// //       .catch(error => console.log(error));
+// //   }, 2000);
+// // });
+
+// function mediaPagination() {
+//   if (window.innerWidth <= 480) {
+//     options.visiblePages = 4;
+//   } else {
+//     options.visiblePages = 7;
 //   }
 // }
